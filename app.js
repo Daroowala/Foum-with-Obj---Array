a6a98d34a1573b4d92df7638d5703e6286178bd1
my name <blueshaperd3@gmail.com>
2024-09-01 01:36:19 +0500

    form done

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 document.getElementById('studentForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
-    const name = document.getElementById('studentName').value;
-    const email = document.getElementById('studentEmail').value;
-    const phone = document.getElementById('studentPhone').value;
-    const course = document.getElementById('studentCourse').value;
+    const firstname = document.getElementById('firstName').value;
+    const lastname = document.getElementById('lastName').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('Email').value;
+    const phone = document.getElementById('Phone').value;
+    const course = document.getElementById('Course').value;
 
-    if (name && email && phone && course) {
+    if (firstname && lastname && age && email && phone && course) {
         document.getElementById('message').innerHTML = `<p>Thank you, ${name}! Your registration for the ${course} course has been received.</p>`;
         document.getElementById('studentForm').reset();
     } else {
